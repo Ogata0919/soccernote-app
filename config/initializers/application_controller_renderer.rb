366904453a4a87876e482709646f88d0feb642ba
Ogata0919 <71375611+Ogata0919@users.noreply.github.com>
2021-03-16 17:02:52 +0900

    修正

--- config/initializers/application_controller_renderer.rb
+++ config/initializers/application_controller_renderer.rb
@@ -6,3 +6,6 @@
 #     https: false
 #   )
 # end
+
+
+Refile.secret_key = 'f76c769cc661b0a30e179be1ef54baea21b604464a6a7dc2743070530a413733cad336754399530de52131a284e11ad7f10dc8ef0cbc35b2528c12060a64aeec'
